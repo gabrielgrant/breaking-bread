ed52a6b615d4ac4ed7f41a58bb3fc560dc0b5508
Gabriel Grant <g@briel.ca>
2020-03-20 13:59:57 -0700

    add initial model fetching in application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import Papa from 'papaparse';
 
 export default class ApplicationRoute extends Route {
+    async model() {
+        let result = await new Promise((resolve, reject) => {
+            const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQKLXMryGmJV8P9ajBwXu5cYsw9982eJdEr1SZPYSPH0OW8_6ewlVBc6CNbTyQNSnjNIHXQo2-FXNVa/pub?gid=0&single=true&output=csv";
+            Papa.parse(url, {
+                download: true,
+                header: true,
+                skipEmptyLines: 'greedy',
+                complete: resolve,
+                error: reject
+            });
+        });
+        return result.data.filterBy('Approved', 'TRUE');
+    }
 }
