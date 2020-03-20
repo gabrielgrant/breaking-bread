65e305dc51b3bf8ab98a4117132aeb0615205237
Gabriel Grant <g@briel.ca>
2020-03-20 14:42:35 -0700

    group restaurants by province

--- app/routes/restaurants.js
+++ app/routes/restaurants.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
+import groupBy from 'lodash/groupBy'
 
 export default class RestaurantsRoute extends Route {
+    model() {
+        return groupBy(this.modelFor('application'), 'Province');
+    }
 }
