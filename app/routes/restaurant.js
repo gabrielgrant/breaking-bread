1b04c71546f100ae64f5b1d528178873c9b92bf9
Gabriel Grant <g@briel.ca>
2020-03-20 16:44:13 -0700

    filter master list in restaurant model hook

--- app/routes/restaurant.js
+++ app/routes/restaurant.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class RestaurantRoute extends Route {
+    model(params) {
+        return this.modelFor('application').filterBy('id', params.restaurant_id)[0];
+    }
 }
