9f6b4283270dba2a1235a2085639628f7a41f5db
Gabriel Grant <g@briel.ca>
2020-03-20 17:43:08 -0700

    switch to using cors-anywhere proxy
    Google sheets CSV endpoint doesn't have CORS enabled

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,7 +5,7 @@ import trim from 'lodash/trim';
 export default class ApplicationRoute extends Route {
     async model() {
         let result = await new Promise((resolve, reject) => {
-            const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQKLXMryGmJV8P9ajBwXu5cYsw9982eJdEr1SZPYSPH0OW8_6ewlVBc6CNbTyQNSnjNIHXQo2-FXNVa/pub?gid=0&single=true&output=csv";
+            const url = "https://cors-anywhere.herokuapp.com/docs.google.com/spreadsheets/d/e/2PACX-1vQKLXMryGmJV8P9ajBwXu5cYsw9982eJdEr1SZPYSPH0OW8_6ewlVBc6CNbTyQNSnjNIHXQo2-FXNVa/pub?gid=0&single=true&output=csv";
             Papa.parse(url, {
                 download: true,
                 header: true,
