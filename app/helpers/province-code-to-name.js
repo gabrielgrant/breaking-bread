84d9357e129f0016417b421b34e2de8cef6cb141
Gabriel Grant <g@briel.ca>
2020-03-21 13:06:56 -0700

    implement province mapping helper

--- app/helpers/province-code-to-name.js
+++ app/helpers/province-code-to-name.js
@@ -1,5 +1,99 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function provinceCodeToName(params/*, hash*/) {
-  return params;
+  return mapping.find(p => p.code === params[0]).name;
 });
+
+var mapping = [
+  {
+    "code": "NL",
+    "name": "Newfoundland and Labrador",
+    "abbreviationEn": "N.L.",
+    "abbreviationFr": "T.-N.-L.",
+    "region": "Atlantic"
+  },
+  {
+    "code": "PE",
+    "name": "Prince Edward Island",
+    "abbreviationEn": "P.E.I.",
+    "abbreviationFr": "Î.-P.-É.",
+    "region": "Atlantic"
+  },
+  {
+    "code": "NS",
+    "name": "Nova Scotia",
+    "abbreviationEn": "N.S.",
+    "abbreviationFr": "N.-É.",
+    "region": "Atlantic"
+  },
+  {
+    "code": "NB",
+    "name": "New Brunswick",
+    "abbreviationEn": "N.B.",
+    "abbreviationFr": "N.-B.",
+    "region": "Atlantic"
+  },
+  {
+    "code": "QC",
+    "name": "Quebec",
+    "abbreviationEn": "Que.",
+    "abbreviationFr": "Qc",
+    "region": "Quebec"
+  },
+  {
+    "code": "ON",
+    "name": "Ontario",
+    "abbreviationEn": "Ont.",
+    "abbreviationFr": "Ont.",
+    "region": "Ontario"
+  },
+  {
+    "code": "MB",
+    "name": "Manitoba",
+    "abbreviationEn": "Man.",
+    "abbreviationFr": "Man.",
+    "region": "Prairies"
+  },
+  {
+    "code": "SK",
+    "name": "Saskatchewan",
+    "abbreviationEn": "Sask.",
+    "abbreviationFr": "Sask.",
+    "region": "Prairies"
+  },
+  {
+    "code": "AB",
+    "name": "Alberta",
+    "abbreviationEn": "Alta.",
+    "abbreviationFr": "Alb.",
+    "region": "Prairies"
+  },
+  {
+    "code": "BC",
+    "name": "British Columbia",
+    "abbreviationEn": "B.C.",
+    "abbreviationFr": "C.-B.",
+    "region": "British Columbia"
+  },
+  {
+    "code": "YT",
+    "name": "Yukon",
+    "abbreviationEn": "Y.T.",
+    "abbreviationFr": "Yn",
+    "region": "Territories"
+  },
+  {
+    "code": "NT",
+    "name": "Northwest Territories",
+    "abbreviationEn": "N.W.T.",
+    "abbreviationFr": "T.N.-O.",
+    "region": "Territories"
+  },
+  {
+    "code": "NU",
+    "name": "Nunavut",
+    "abbreviationEn": "Nvt.",
+    "abbreviationFr": "Nt",
+    "region": "Territories"
+  }
+];
