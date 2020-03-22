2cb0ccadc1285329baa7b810eca3151df04f4ea9
Gabriel Grant <g@briel.ca>
2020-03-22 12:16:16 -0700

    add restaurant filtering by province

--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -1,9 +1,19 @@
 import Controller from '@ember/controller';
 import groupBy from 'lodash/groupBy'
+import { tracked } from '@glimmer/tracking';
 
 export default class RestaurantsController extends Controller {
+    queryParams = ['province'];
+    @tracked province = null;
+    get filteredRestaurants() {
+        if (this.province) {
+            return this.model.filter(r => r.Province === this.province);
+        } else {
+            return this.model;
+        }
+    }
     get sortedRestaurants() {
-        let byProvince = groupBy(this.model, 'Province');
+        let byProvince = groupBy(this.filteredRestaurants, 'Province');
         let byCityEntries = Object.entries(byProvince).map(([k, v]) => [k, groupBy(v, 'City')]);
         return Object.fromEntries(byCityEntries);
     }
