96f8a695c774e683ccce11ad0bd8dfd7dffb8006
Gabriel Grant <g@briel.ca>
2020-03-21 23:07:28 -0700

    implement action in index controller to update the restaurant count on the squarespace homepage after iframe load
    uses a postMessage call that is recieved by this JS on the squarespace page:
    
    window.addEventListener('message', function(e) {
        if (!e.data.restaurantCount) return;
        Array.from(document.querySelectorAll('h3')).find(node => node.textContent.endsWith('restaurants and counting…')).textContent = `${e.data.restaurantCount} restaurants and counting…`;
    } , false);

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,9 @@
 import Controller from '@ember/controller';
+import { action } from "@ember/object";
 
 export default class IndexController extends Controller {
+    @action
+    resetIFrameCount(e) {
+        e.target.contentWindow.postMessage({restaurantCount: this.model.length}, '*');
+    }
 }
