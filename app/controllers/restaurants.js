bea1cc666e60b0f6f805f80f29dec49e312c4eb1
Gabriel Grant <g@briel.ca>
2020-03-22 10:52:27 -0700

    move restaurant list sorting to restaurants controller (from model hook)

--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -1,4 +1,10 @@
 import Controller from '@ember/controller';
+import groupBy from 'lodash/groupBy'
 
 export default class RestaurantsController extends Controller {
+    get sortedRestaurants() {
+        let byProvince = groupBy(this.model, 'Province');
+        let byCityEntries = Object.entries(byProvince).map(([k, v]) => [k, groupBy(v, 'City')]);
+        return Object.fromEntries(byCityEntries);
+    }
 }
--- app/routes/restaurants.js
+++ app/routes/restaurants.js
@@ -1,10 +1,4 @@
 import Route from '@ember/routing/route';
-import groupBy from 'lodash/groupBy'
 
 export default class RestaurantsRoute extends Route {
-    model() {
-        let byProvince = groupBy(this.modelFor('application'), 'Province');
-        let byCityEntries = Object.entries(byProvince).map(([k, v]) => [k, groupBy(v, 'City')]);
-        return Object.fromEntries(byCityEntries);
-    }
 }
