1299105da784b7e4221c2f82353c8bad250563dd
Gabriel Grant <g@briel.ca>
2020-03-23 15:31:32 -0700

    add some redirects

--- app/routes/404.js
+++ app/routes/404.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
 
-export default class 404Route extends Route {
+export default class NotFoundRoute extends Route {
+    model(params){
+        console.log(params);
+        switch (params.path) {
+            case 'get-involved':
+                this.transitionTo('signup');
+                break;
+            case 'participants':
+                this.transitionTo('restaurants');
+                break;
+            case 'restaurants/all':
+                this.transitionTo('restaurants');
+                break;
+            case 'british-columbia':
+                this.transitionTo('restaurants', {queryParams: {province: 'BC'}});
+                break;
+        }
+    }
 }
