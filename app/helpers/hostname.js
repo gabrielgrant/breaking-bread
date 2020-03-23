a29d119483fc719c6a0504f708cd1ee800e8bcdd
Gabriel Grant <g@briel.ca>
2020-03-22 22:07:44 -0700

    add http:// to url if missing in hostname helper

--- app/helpers/hostname.js
+++ app/helpers/hostname.js
@@ -1,5 +1,5 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function hostname(params/*, hash*/) {
-  return new URL(params[0]).hostname;
+  return new URL((params[0].startsWith('http') ? '':'http://') + params[0]).hostname;
 });
